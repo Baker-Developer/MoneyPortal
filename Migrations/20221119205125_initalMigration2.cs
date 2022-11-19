184b6f4cf603824e5fba2eb83ace46df3f2cd1a3
Baker-Developer <benjaminbaker1122@gmail.com>
2022-11-19 14:53:43 -0600

    PostgreSQL setup

---
+++ Migrations/20221119205125_initalMigration2.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MoneyPortal.Migrations
+{
+    public partial class initalMigration2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
