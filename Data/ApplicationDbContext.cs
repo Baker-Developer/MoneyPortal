6142feaaa9fc88065a33c90629562543fe37e185
Baker-Developer <benjaminbaker1122@gmail.com>
2022-11-21 00:01:33 -0600

    initial setup

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using MoneyPortal.Models;
 
 namespace MoneyPortal.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<MoneyPortalUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
