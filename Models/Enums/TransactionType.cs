ca67503521193f728322326deffa6e1747d997ae
Baker-Developer <benjaminbaker1122@gmail.com>
2022-12-30 17:31:18 -0600

    starting from beginning models

--- Models/Enums/TransactionType.cs
+++ Models/Enums/TransactionType.cs
@@ -1,6 +1,21 @@
-﻿namespace MoneyPortal.Models.Enums
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace MoneyPortal.Models.Enums
 {
     public enum TransactionType
     {
+        [Display(Name = "Deposit")]
+        Deposit,
+        [Display(Name = "Cash Withdawal")]
+        CashWithdawal,
+        [Display(Name = "Point Of Sale")]
+        PointOfSale,
+        [Display(Name = "Card Number Entered")]
+        CardNumberEntered,
+        [Display(Name = "Check")]
+        Check,
+        [Display(Name = "Transfer")]
+        Transfer
     }
 }
--- Models/Enums/UserRoles.cs
+++ Models/Enums/UserRoles.cs
@@ -2,6 +2,12 @@
 {
     public enum UserRoles
     {
+        /*
+         * Roles Permissions
+         * Owner - Has full access to invite members & create budgets and can delete the household
+         * Member - Has ablility inside an invited household to personal bank accounts to household
+         * Personal - 
+         */
         Owner,
         Member,
         Personal
